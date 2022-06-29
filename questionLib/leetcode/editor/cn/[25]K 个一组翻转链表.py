c5a1f86f311a749eb4cc96e292d3e2787a12127a
Li Shanglin <1958698497@qq.com>
2022-06-29 22:53:05 +0800

    0629

---
+++ questionLib/leetcode/editor/cn/[25]K 个一组翻转链表.py
@@ -0,0 +1,83 @@
+# 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。 
+# 
+#  k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。 
+# 
+#  你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  
+# 输入：head = [1,2,3,4,5], k = 2
+# 输出：[2,1,4,3,5]
+#  
+# 
+#  示例 2： 
+# 
+#  
+# 
+#  
+# 输入：head = [1,2,3,4,5], k = 3
+# 输出：[3,2,1,4,5]
+#  
+# 
+#  
+# 提示：
+# 
+#  
+#  链表中的节点数目为 n 
+#  1 <= k <= n <= 5000 
+#  0 <= Node.val <= 1000 
+#  
+# 
+#  
+# 
+#  进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？ 
+# 
+#  
+#  
+#  Related Topics 递归 链表 👍 1690 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    # 翻转一个子链表，并且返回新的头与尾
+    def reverse(self, head: ListNode, tail: ListNode):
+        prev = tail.next
+        p = head
+        while prev != tail:
+            nex = p.next
+            p.next = prev
+            prev = p
+            p = nex
+        return tail, head
+
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        hair = ListNode(0)
+        hair.next = head
+        pre = hair
+
+        while head:
+            tail = pre
+            # 查看剩余部分长度是否大于等于 k
+            for i in range(k):
+                tail = tail.next
+                if not tail:
+                    return hair.next
+            nex = tail.next
+            head, tail = self.reverse(head, tail)
+            # 把子链表重新接回原链表
+            pre.next = head
+            tail.next = nex
+            pre = tail
+            head = tail.next
+
+        return hair.next
+
+# leetcode submit region end(Prohibit modification and deletion)
