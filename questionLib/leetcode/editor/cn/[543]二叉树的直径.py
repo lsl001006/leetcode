ea1089fa123079f6d6f242d2e02746c741450e6e
Li Shanglin <1958698497@qq.com>
2022-07-13 14:33:46 +0800

    0713 树 简单
    Signed-off-by: Li Shanglin <1958698497@qq.com>

---
+++ questionLib/leetcode/editor/cn/[543]二叉树的直径.py
@@ -0,0 +1,50 @@
+# 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。 
+# 
+#  
+# 
+#  示例 : 
+# 给定二叉树 
+# 
+#            1
+#          / \
+#         2   3
+#        / \     
+#       4   5    
+#  
+# 
+#  返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。 
+# 
+#  
+# 
+#  注意：两结点之间的路径长度是以它们之间边的数目表示。 
+#  Related Topics 树 深度优先搜索 二叉树 👍 1092 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 1
+
+        def depth(node):
+            # 访问到空节点了，返回0
+            if not node:
+                return 0
+            # 左儿子为根的子树的深度
+            L = depth(node.left)
+            # 右儿子为根的子树的深度
+            R = depth(node.right)
+            # 计算d_node即L+R+1 并更新ans
+            self.ans = max(self.ans, L + R + 1)
+            # 返回该节点为根的子树的深度
+            return max(L, R) + 1
+
+        depth(root)
+        return self.ans - 1
+
+# leetcode submit region end(Prohibit modification and deletion)
