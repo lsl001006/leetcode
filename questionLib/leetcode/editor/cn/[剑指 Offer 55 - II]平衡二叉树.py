699b30f2b74055ec158a43e12baafe3e1ee7db5b
Li Shanglin <1958698497@qq.com>
2022-07-15 21:27:51 +0800

    0715 树 简单
    Signed-off-by: Li Shanglin <1958698497@qq.com>

---
+++ questionLib/leetcode/editor/cn/[剑指 Offer 55 - II]平衡二叉树.py
@@ -0,0 +1,73 @@
+# 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。 
+# 
+#  
+# 
+#  示例 1: 
+# 
+#  给定二叉树 [3,9,20,null,null,15,7] 
+# 
+#  
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7 
+# 
+#  返回 true 。 
+#  
+# 示例 2: 
+# 
+#  给定二叉树 [1,2,2,3,3,null,null,4,4] 
+# 
+#  
+#        1
+#       / \
+#      2   2
+#     / \
+#    3   3
+#   / \
+#  4   4
+#  
+# 
+#  返回 false 。 
+# 
+#  
+# 
+#  限制： 
+# 
+#  
+#  0 <= 树的结点个数 <= 10000 
+#  
+# 
+#  注意：本题与主站 110 题相同：https://leetcode-cn.com/problems/balanced-binary-tree/ 
+# 
+#  
+#  Related Topics 树 深度优先搜索 二叉树 👍 292 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def findDepth(self, root):
+        if not root:
+            return 0
+        return max(self.findDepth(root.left),
+                   self.findDepth(root.right)) + 1
+
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        L = self.findDepth(root.left)
+        R = self.findDepth(root.right)
+        if abs(L - R) > 1:
+            return False
+
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+# leetcode submit region end(Prohibit modification and deletion)
