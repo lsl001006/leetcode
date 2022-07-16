6c7a4a8ec55bd14c4b7b835b6207f93f4f1e03bd
Li Shanglin <1958698497@qq.com>
2022-07-16 15:21:53 +0800

    0715 链表 简单
    Signed-off-by: Li Shanglin <1958698497@qq.com>

---
+++ questionLib/leetcode/editor/cn/[剑指 Offer II 024]反转链表.py
@@ -0,0 +1,64 @@
+# 给定单链表的头节点 head ，请反转链表，并返回反转后的链表的头节点。 
+# 
+#  
+#  
+#  
+# 
+#  示例 1： 
+# 
+#  
+# 输入：head = [1,2,3,4,5]
+# 输出：[5,4,3,2,1]
+#  
+# 
+#  示例 2： 
+# 
+#  
+# 输入：head = [1,2]
+# 输出：[2,1]
+#  
+# 
+#  示例 3： 
+# 
+#  
+# 输入：head = []
+# 输出：[]
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  链表中节点的数目范围是 [0, 5000] 
+#  -5000 <= Node.val <= 5000 
+#  
+# 
+#  
+# 
+#  进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+#  
+#  
+# 
+#  
+# 
+#  注意：本题与主站 206 题相同： https://leetcode-cn.com/problems/reverse-linked-list/ 
+#  Related Topics 递归 链表 👍 78 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, cur = None, head
+        while cur:
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
+        return prev
+# leetcode submit region end(Prohibit modification and deletion)
