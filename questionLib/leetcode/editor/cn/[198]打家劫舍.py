637c9adf2e50fb6a11436170bd81cc39013986de
Li Shanglin <1958698497@qq.com>
2022-08-06 11:29:52 +0800

    0806 动态规划 中等
    Signed-off-by: Li Shanglin <1958698497@qq.com>

--- questionLib/leetcode/editor/cn/[198]打家劫舍.py
+++ questionLib/leetcode/editor/cn/[198]打家劫舍.py
@@ -40,9 +40,8 @@ class Solution:
         n = len(nums)
         if n <= 2:
             return max(nums)
-        dp = [0] * n
-        dp[0], dp[1] = nums[0], max(nums[1], nums[0])
+        prev, cur = nums[0], max(nums[1], nums[0])
         for i in range(2, n):
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
-        return dp[-1]
+            prev, cur = cur, max(prev + nums[i], cur)
+        return cur
 # leetcode submit region end(Prohibit modification and deletion)
