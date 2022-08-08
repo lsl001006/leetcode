ead63ff2361f36c17a7046fd21b6e4331cafbce3
Li Shanglin <1958698497@qq.com>
2022-08-08 11:11:29 +0800

    0808 链表 中等
    Signed-off-by: Li Shanglin <1958698497@qq.com>

---
+++ questionLib/leetcode/editor/cn/[61]旋转链表.py
@@ -0,0 +1,61 @@
+# 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。 
+# 
+#  
+# 
+#  示例 1： 
+#  
+#  
+# 输入：head = [1,2,3,4,5], k = 2
+# 输出：[4,5,1,2,3]
+#  
+# 
+#  示例 2： 
+#  
+#  
+# 输入：head = [0,1,2], k = 4
+# 输出：[2,0,1]
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  链表中节点的数目在范围 [0, 500] 内 
+#  -100 <= Node.val <= 100 
+#  0 <= k <= 2 * 10⁹ 
+#  
+# 
+#  Related Topics 链表 双指针 👍 809 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if k == 0 or not head or not head.next:
+            return head
+
+        n = 1
+        cur = head
+        while cur.next:
+            cur = cur.next
+            n += 1
+
+        if (add := n - k % n) == n:
+            return head
+
+        cur.next = head
+        while add:
+            cur = cur.next
+            add -= 1
+
+        ret = cur.next
+        cur.next = None
+        return ret
+
+# leetcode submit region end(Prohibit modification and deletion)
