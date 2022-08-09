0750c9f09d455d913e19a98b1e62e49982935095
Li Shanglin <1958698497@qq.com>
2022-08-09 12:22:40 +0800

    0809 动态规划 中等
    Signed-off-by: Li Shanglin <1958698497@qq.com>

---
+++ questionLib/leetcode/editor/cn/[剑指 Offer 47]礼物的最大价值.py
@@ -0,0 +1,41 @@
+# 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直
+# 到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？ 
+# 
+#  
+# 
+#  示例 1: 
+# 
+#  输入: 
+# [
+#   [1,3,1],
+#   [1,5,1],
+#   [4,2,1]
+# ]
+# 输出: 12
+# 解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物 
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  0 < grid.length <= 200 
+#  0 < grid[0].length <= 200 
+#  
+# 
+#  Related Topics 数组 动态规划 矩阵 👍 317 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def maxValue(self, grid: list[list[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        grid = [[0] * (n + 1)] + grid
+        for i in range(m + 1):
+            grid[i].insert(0, 0)
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                grid[i][j] += max(grid[i - 1][j], grid[i][j - 1])
+        return grid[-1][-1]
+# leetcode submit region end(Prohibit modification and deletion)
