1e81abf355bd31618a6d5f62fa8983bf3debac0e
Li Shanglin <1958698497@qq.com>
2022-08-11 22:38:54 +0800

    0810 链表 中等
    Signed-off-by: Li Shanglin <1958698497@qq.com>

---
+++ questionLib/leetcode/editor/cn/[707]设计链表.py
@@ -0,0 +1,169 @@
+# 设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。val 是当前节点的值，next 是指向下一个节点的指针
+# /引用。如果要使用双向链表，则还需要一个属性 prev 以指示链表中的上一个节点。假设链表中的所有节点都是 0-index 的。 
+# 
+#  在链表类中实现这些功能： 
+# 
+#  
+#  get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。 
+#  addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。 
+#  addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。 
+#  addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val 的节点。如果 index 等于链表的长度，则该节点将附加
+# 到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。 
+#  deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。 
+#  
+# 
+#  
+# 
+#  示例： 
+# 
+#  MyLinkedList linkedList = new MyLinkedList();
+# linkedList.addAtHead(1);
+# linkedList.addAtTail(3);
+# linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+# linkedList.get(1);            //返回2
+# linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+# linkedList.get(1);            //返回3
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  所有val值都在 [1, 1000] 之内。 
+#  操作次数将在 [1, 1000] 之内。 
+#  请不要使用内置的 LinkedList 库。 
+#  
+# 
+#  Related Topics 设计 链表 👍 522 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next, self.prev = None, None
+
+
+class MyLinkedList:
+    def __init__(self):
+        self.size = 0
+        # sentinel nodes as pseudo-head and pseudo-tail
+        self.head, self.tail = ListNode(0), ListNode(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        # if index is invalid
+        if index < 0 or index >= self.size:
+            return -1
+
+        # choose the fastest way: to move from the head
+        # or to move from the tail
+        if index + 1 < self.size - index:
+            curr = self.head
+            for _ in range(index + 1):
+                curr = curr.next
+        else:
+            curr = self.tail
+            for _ in range(self.size - index):
+                curr = curr.prev
+
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        pred, succ = self.head, self.head.next
+
+        self.size += 1
+        to_add = ListNode(val)
+        to_add.prev = pred
+        to_add.next = succ
+        pred.next = to_add
+        succ.prev = to_add
+
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        succ, pred = self.tail, self.tail.prev
+
+        self.size += 1
+        to_add = ListNode(val)
+        to_add.prev = pred
+        to_add.next = succ
+        pred.next = to_add
+        succ.prev = to_add
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        # If index is greater than the length,
+        # the node will not be inserted.
+        if index > self.size:
+            return
+
+        # [so weird] If index is negative,
+        # the node will be inserted at the head of the list.
+        if index < 0:
+            index = 0
+
+        # find predecessor and successor of the node to be added
+        if index < self.size - index:
+            pred = self.head
+            for _ in range(index):
+                pred = pred.next
+            succ = pred.next
+        else:
+            succ = self.tail
+            for _ in range(self.size - index):
+                succ = succ.prev
+            pred = succ.prev
+
+        # insertion itself
+        self.size += 1
+        to_add = ListNode(val)
+        to_add.prev = pred
+        to_add.next = succ
+        pred.next = to_add
+        succ.prev = to_add
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        # if the index is invalid, do nothing
+        if index < 0 or index >= self.size:
+            return
+
+        # find predecessor and successor of the node to be deleted
+        if index < self.size - index:
+            pred = self.head
+            for _ in range(index):
+                pred = pred.next
+            succ = pred.next.next
+        else:
+            succ = self.tail
+            for _ in range(self.size - index - 1):
+                succ = succ.prev
+            pred = succ.prev.prev
+
+        # delete pred.next
+        self.size -= 1
+        pred.next = succ
+        succ.prev = pred
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
+# leetcode submit region end(Prohibit modification and deletion)
