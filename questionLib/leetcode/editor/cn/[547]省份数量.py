d38bad2fa2344a7f389cbb4f72d351a475db74eb
Li Shanglin <1958698497@qq.com>
2022-08-11 14:45:08 +0800

    0810 并查集 中等
    Signed-off-by: Li Shanglin <1958698497@qq.com>

---
+++ questionLib/leetcode/editor/cn/[547]省份数量.py
@@ -0,0 +1,93 @@
+# 
+#  
+#  有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连
+# 。 
+#  
+#  
+# 
+#  省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。 
+# 
+#  给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 
+# isConnected[i][j] = 0 表示二者不直接相连。 
+# 
+#  返回矩阵中 省份 的数量。 
+# 
+#  
+# 
+#  示例 1： 
+#  
+#  
+# 输入：isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+# 输出：2
+#  
+# 
+#  示例 2： 
+#  
+#  
+# 输入：isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+# 输出：3
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  1 <= n <= 200 
+#  n == isConnected.length 
+#  n == isConnected[i].length 
+#  isConnected[i][j] 为 1 或 0 
+#  isConnected[i][i] == 1 
+#  isConnected[i][j] == isConnected[j][i] 
+#  
+# 
+#  Related Topics 深度优先搜索 广度优先搜索 并查集 图 👍 846 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class UnionFind:
+    def __init__(self):
+        self.father = {}
+        # 额外记录集合的数量
+        self.num_of_sets = 0
+
+    def find(self, x):
+        root = x
+
+        while self.father[root] != None:
+            root = self.father[root]
+
+        while x != root:
+            original_father = self.father[x]
+            self.father[x] = root
+            x = original_father
+
+        return root
+
+    def merge(self, x, y):
+        root_x, root_y = self.find(x), self.find(y)
+
+        if root_x != root_y:
+            self.father[root_x] = root_y
+            # 集合的数量-1
+            self.num_of_sets -= 1
+
+    def add(self, x):
+        if x not in self.father:
+            self.father[x] = None
+            # 集合的数量+1
+            self.num_of_sets += 1
+
+
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        uf = UnionFind()
+        for i in range(len(M)):
+            uf.add(i)
+            for j in range(i):
+                if M[i][j]:
+                    uf.merge(i, j)
+
+        return uf.num_of_sets
+
+# leetcode submit region end(Prohibit modification and deletion)
